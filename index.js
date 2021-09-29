c89745fcda340231815cb42e64da707f9c516316
ash m <ashkhmu@gmail.com>
2021-09-29 21:19:16 +0300

    finished t1 t2 t3

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ let tasks = [];
 let categories = [];
 
 // REMOVE ME: SAMPLE FILLING
-// tasks = [
-//   { id: 0, title: "Game of thrones", category: "Movies", done: false },
-//   { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-// ];
+tasks = [
+  { id: 0, title: "Game of thrones", category: "Movies", done: false },
+  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
+];
 
 categories = ["Movies", "Groceries"];
+
 // SAMPLE
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
@@ -20,13 +21,17 @@ renderTasks(tasks, "tasks-list");
 function taskChecked(taskId, checked) {
   // implement the delete task.
   // You are given the task id
+  a = tasks.find((task) => task.id === taskId);
+  a.done = checked;
+
+  renderTasks(tasks, "tasks-list");
   console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
 function addTask() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
   const taskTitle = getNewTaskText();
-  const idNumber = tasks.length++;
+  const idNumber = tasks.length;
   const task = {
     id: idNumber,
     title: taskTitle,
@@ -36,13 +41,18 @@ function addTask() {
   tasks.push(task);
   renderTasks(tasks, "tasks-list");
 
-  alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
+  // alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
-  alert(`New category was added: ${newCategory}`);
+  categories.push(newCategory);
+
+  renderCategories(categories, "categories-list");
+  renderCategories(categories, "categories-list-filter");
+
+  // alert(`New category was added: ${newCategory}`);
 }
 
 function filterTasks() {
