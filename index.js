1dddf6dfad3bf991450a461b46ab847a84eb4a2a
CaptSoulBeard <Salim.Al.Ateeqi1@gmail.com>
2021-09-30 19:56:02 +0300

    Completed the ToDo List

--- index.js
+++ index.js
@@ -5,22 +5,11 @@ const CATEGORY_FILTER = "categories-list-filter";
 let tasks = [];
 let categories = [];
 
-// REMOVE ME: SAMPLE FILLING
-// tasks = [
-//   { id: 0, title: "Game of thrones", category: "Movies", done: false },
-//   { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-// ];
-
-// categories = ["Movies", "Groceries"];
-
-// SAMPLE
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
 renderTasks(tasks, "tasks-list");
 
 function taskChecked(taskId, checked) {
-  // implement the delete task.
-  // You are given the task id
   a = tasks.find((task) => task.id === taskId);
   a.done = checked;
 
@@ -40,26 +29,22 @@ function addTask() {
   };
   tasks.push(task);
   renderTasks(tasks, "tasks-list");
-
-  // alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
 }
 
 function addCategory() {
   const newCategory = getNewCategoryText();
-  // continue the code here
   categories.push(newCategory);
-
   renderCategories(categories, "categories-list");
   renderCategories(categories, "categories-list-filter");
-
-  // alert(`New category was added: ${newCategory}`);
 }
 
 function filterTasks() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
   const done = getFilteredDone();
-  // continue the code here
 
-  // REMOVE ME: sample alert
-  alert(`Category: ${selectedCategory} | done: ${done}`);
+  complete = tasks.filter(
+    (task) => task.category === selectedCategory && task.done === done
+  );
+
+  renderTasks(complete, "tasks-list");
 }
